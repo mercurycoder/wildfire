4434934875e0a665cbb1ae8aaf55c7627e04be96
Lukas Palmer <prometheusinnovations@gmail.com>
2015-06-25 10:15:18 -0400

    Did a thing

---
+++ js/graphics.js
@@ -0,0 +1,17 @@
+﻿var canvas;
+var gl;
+var windowWidth;
+var windowHeight;
+function setupWebGL() {
+    canvas = document.getElementById('gameCanvas');
+    gl = canvas.getContext('experimental-webgl');
+    gl.clearColor(0.3, 0.4, 0.5, 1);
+}
+function updateWebGL() {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+    canvas.width = windowWidth;
+    canvas.height = windowHeight;
+    gl.viewport(0, 0, windowWidth, windowHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
---
+++ js/wildfire.js
@@ -0,0 +1,4 @@
+﻿function setup() {
+    setupWebGL();
+    updateWebGL();
+}
